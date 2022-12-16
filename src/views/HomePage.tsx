6a0500490548b2a69c2d306fc4fd668da0e953b0
fpatr <fpatr@qq.com>
2022-12-16 19:34:15 +0800

    初步完成页面和 css 结构

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -7,42 +7,44 @@ export const HomePage = defineComponent({
   setup: (props, context) => {
     return () => (
       <>
-        <header class="top_header">
-          <div class="top_div">
-            <div class="activate_reset">
-              <svg class="icon" style=" width: 3em; height: 3em;"></svg>
-              <span class="text">清空缓存</span>
+        <div class={s.wrapper}>
+          <header class={s.top_header}>
+            <div class={s.top_div}>
+              <div class={s.activate_reset}>
+                <svg class={s.icon} style=" width: 3em; height: 3em;"></svg>
+                <span class={s.text}>清空缓存</span>
+              </div>
+              <div class={s.activate_delete}>
+                <svg class={s.icon} style=" width: 3em; height: 3em;"></svg>
+                <span class={s.text}>删除</span>
+              </div>
             </div>
-            <div class="activate_delete">
-              <svg class="icon" style=" width: 3em; height: 3em;"></svg>
-              <span class="text">删除</span>
+            <div class={s.div_fm}>
+              <form
+                class="fm"
+                method="get"
+                action="https://www.baidu.com/s"
+                target="_blank"
+              >
+                <input name="wd" type="text" class={s.input_search}></input>
+                <button type="submit" class={s.button_search}>
+                  百度搜索
+                </button>
+              </form>
             </div>
-          </div>
-          <div class="div_fm">
-            <form
-              class="fm"
-              method="get"
-              action="https://www.baidu.com/s"
-              target="_blank"
-            >
-              <input name="wd" type="text" class="input_search"></input>
-              <button type="submit" class="button_search">
-                百度搜索
-              </button>
-            </form>
-          </div>
-        </header>
+          </header>
 
-        <main class="top_main">
-          <div class="icon_wrapper">
-            <div class="add_url icon_Click">
-              <span class="icon_icon">
-                <svg class="icon"></svg>
-              </span>
-              <div class="text">新增网址</div>
+          <main class={s.top_main}>
+            <div class={s.icon_wrapper}>
+              <div class="add_url icon_Click">
+                <span class={s.icon_icon}>
+                  +<svg class={s.icon}></svg>
+                </span>
+                <div class={s.text}>新增网址</div>
+              </div>
             </div>
-          </div>
-        </main>
+          </main>
+        </div>
       </>
     );
   },
